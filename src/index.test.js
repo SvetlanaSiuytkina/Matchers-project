65a9b6b2539b5d1a527892da6c9ec1fe22d4961c
SVETA <svetadan1234@mail.ru>
2026-05-13 23:16:44 +0500

    Initial commit

---
+++ src/index.test.js
@@ -0,0 +1,31 @@
+import { sortHeroesHealth } from '../src/index';
+
+test('sorts the heroes in descending order of health', () => {
+    const input = [
+        {name: 'мечник', health: 10},
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80}
+    ]
+
+    const expected = [
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+        {name: 'мечник', health: 10},
+    ]
+
+    expect(sortHeroesHealth(input)).toEqual(expected);
+});
+
+test('processes an empty array', () => {
+    expect(sortHeroesHealth([])).toEqual([]);
+});
+
+test('sorts an array with the same health values', () => {
+    const input = [
+        {name: 'мечник', health: 10},
+        {name: 'маг', health: 10},
+        {name: 'лучник', health: 10}
+    ]
+
+    expect(sortHeroesHealth(input)).toEqual(input);
+});
